f8e04191c573311fc754976ba693af12b50a6573
raphaelstary <raphael.stary@gmail.com>
2016-09-09 10:12:05 +0200

    add key and pad controls to touch tutorial

--- www/js/mygame/scenes/Tutorial.js
+++ www/js/mygame/scenes/Tutorial.js
@@ -1,7 +1,8 @@
-G.Tutorial = (function () {
+G.Tutorial = (function (Key, Event) {
     "use strict";
 
-    function Tutorial() {
+    function Tutorial(services) {
+        this.events = services.events;
     }
 
     //noinspection JSUnusedGlobalSymbols
@@ -13,5 +14,35 @@ G.Tutorial = (function () {
     Tutorial.prototype.skipDown = function () {
     };
 
+    Tutorial.prototype.postConstruct = function () {
+        this.itIsOver = false;
+        var self = this;
+        this.keyListener = this.events.subscribe(Event.KEY_BOARD, function (keyBoard) {
+            if (self.itIsOver)
+                return;
+
+            if (keyBoard[Key.ENTER] || keyBoard[Key.SPACE]) {
+                self.itIsOver = true;
+                self.nextScene();
+            }
+        });
+
+        this.gamePadListener = this.events.subscribe(Event.GAME_PAD, function (gamePad) {
+            if (self.itIsOver)
+                return;
+
+            if (gamePad.isAPressed() || gamePad.isStartPressed()) {
+                self.itIsOver = true;
+                self.nextScene();
+            }
+        });
+
+    };
+
+    Tutorial.prototype.preDestroy = function () {
+        this.events.unsubscribe(this.keyListener);
+        this.events.unsubscribe(this.gamePadListener);
+    };
+
     return Tutorial;
-})();
+})(H5.Key, H5.Event);
