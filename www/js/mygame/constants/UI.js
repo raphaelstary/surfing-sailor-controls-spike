6fed39c6aa5f1ff23a6aad3b4beec6a309189ae3
raphaelstary <raphael.stary@gmail.com>
2016-09-17 11:26:46 +0200

    new color scheme part 2

--- www/js/mygame/constants/UI.js
+++ www/js/mygame/constants/UI.js
@@ -4,18 +4,18 @@ G.UI = {
     WHITE: '#f4f4f4',
     BLACK: '#434343',
 
-    WALL_COLOR: '#8430bf', // h:275 s:75, b:75
+    WALL_COLOR: '#806080', // s:25, b:50
 
-    BALL_COLOR: '#bf3083',
-    BALL_SHADOW_COLOR: '#802058',
+    BALL_COLOR: '#bf60bf', // s:50, b:75
+    BALL_SHADOW_COLOR: '#804080', // s:50, b:50
     BALL_HIGHLIGHT_COLOR: '#f4f4f4',
 
-    PLAYER_COLOR: '#bf30bf', // h:300 s:75, b:75
+    PLAYER_COLOR: '#bf30bf', // s:75, b:75
     PLAYER_SHADOW_COLOR: '#802080', // s:75, b:50
     PLAYER_FRAME_COLOR: '#f4f4f4',
 
-    SCREEN_HIGHLIGHT_COLOR: '#804080', // S:50, B:50
-    SCREEN_COLOR: '#401040', // s:75, b:25
+    SCREEN_HIGHLIGHT_COLOR: '#754080', // S:50, B:50
+    SCREEN_COLOR: '#381040', // s:75, b:25
 
     PARTICLE_COLOR: '#f4f4f4',
 
